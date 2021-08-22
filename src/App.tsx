c83296238f7d7eb85050dbeb61618f2525edfb38
jaemushim <shimwoan@gmail.com>
2021-08-22 22:05:54 +0900

    upload img, css loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import './style.css'
+import IMAGE from './react.png'
+
 export const App= ()=>{
-    return <h1>Lorem ipsum dolor sit amet. starter</h1>
+    return <>
+    <h1>Lorem ipsum dolor sit amet. starter</h1>
+    <img src={IMAGE} alt="" />
+    </>
 }
