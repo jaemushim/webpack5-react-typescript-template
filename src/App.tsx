00b7dd6fc6eefe31c1234a5f585495d37f11f0be
jaemushim <shimwoan@gmail.com>
2021-08-22 22:59:32 +0900

    import 에러

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+
+import {Test} from './Test'
+
 import './style.css'
 import IMAGE from './react.png'
+
 
 export const App= ()=>{
     return <>
     <h1>Lorem ipsum dolor sit amet. starter</h1>
     <img src={IMAGE} alt="" />
+    <Test />
     </>
 }
