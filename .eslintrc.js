bbc19ee2bf789575fdd04824b9fac221ec91396d
jaemushim <shimwoan@gmail.com>
2021-08-24 00:25:13 +0900

    바벨, Eslint 설정파일 js 확장자로 변환
    바벨, Eslint 설정파일 js 확장자로 변환

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb-typescript', 'prettier', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
